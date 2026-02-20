712978a6b555dacbaf0e4a71e194ed9f84d8313c
Sandro Andrade <sandro.andrade95@hotmail.com>
2026-02-20 17:21:40 -0300

    ssh private key

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -2,7 +2,7 @@ output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
 
-output "private_key" {
+output "ssh_private_key" {
   value     = tls_private_key.ssh.private_key_pem
   sensitive = true
 }
