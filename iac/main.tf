a976ef94f24e8f6e9cf9a665c51e711d3173a501
Sandro Andrade <sandro.andrade95@hotmail.com>
2026-02-20 15:40:44 -0300

    Adjusting IP

--- iac/main.tf
+++ iac/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_security_rule" "ssh" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  source_address_prefix       = var.my_ip
+  source_address_prefix       = "${var.my_ip}/32"
   destination_port_range      = "22"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
