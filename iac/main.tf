0b8e14dae5848100e4bccfcf06eea0b9a1a4fab5
Sandro Andrade <sandro.andrade95@hotmail.com>
2026-02-20 15:35:49 -0300

    Adjusting NSG

--- iac/main.tf
+++ iac/main.tf
@@ -34,8 +34,10 @@ resource "azurerm_network_security_rule" "ssh" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
+  source_port_range           = "*"
   source_address_prefix       = var.my_ip
   destination_port_range      = "22"
+  destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
@@ -46,8 +48,10 @@ resource "azurerm_network_security_rule" "http" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
+  source_port_range           = "*"
   source_address_prefix       = "*"
   destination_port_range      = "80"
+  destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
