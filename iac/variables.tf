9980d32d7fa56f8ba63c83260f867a4b6ec5fc57
Sandro Andrade <sandro.andrade95@hotmail.com>
2026-02-20 15:21:42 -0300

    subscriptionID

--- iac/variables.tf
+++ iac/variables.tf
@@ -6,3 +6,9 @@ variable "my_ip" {
   description = "Your public IP for SSH"
   type        = string
 }
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type        = string
+  default     = "5c32571a-bb02-413d-9349-fa99ce5ba596"  
+}
