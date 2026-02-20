ccf87471d3f6c3b4a7b1a93b45a271f9fdc0fc08
Sandro Andrade <sandro.andrade95@hotmail.com>
2026-02-20 19:01:28 -0300

    Fixing Subnet association

--- iac/network.tf
+++ iac/network.tf
@@ -18,6 +18,10 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
 resource "azurerm_network_security_rule" "ssh" {
   name                        = "Allow-SSH"
